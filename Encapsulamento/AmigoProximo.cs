1118b00f0b6affd6ad1b0bf1f2d50619895abc54
Douglas Fonseca <douglasfonseca2010@hotmail.com>
2021-11-05 16:44:29 -0400

    Encapsulamento

---
+++ Encapsulamento/AmigoProximo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    class AmigoProximo
+    {
+        public readonly SubCelebridade amiga = new SubCelebridade();
+
+        public void MeusAcessos()
+        {
+            Console.WriteLine("Amigo proximo...");
+
+            Console.WriteLine(amiga.InfoPublica);
+            //Console.WriteLine(CorDosOlhos);
+            //Console.WriteLine(amiga.NumeroCelular);
+            Console.WriteLine(amiga.JeitoDeFalar);
+            //Console.WriteLine(SegredoFamilia);
+            //Console.WriteLine(UsaMuitoPhotoshop);
+        }
+    }
+}
